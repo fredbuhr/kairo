e9db9e7a8fd86c978e97d2febe4fdb2eeb48c54d
fredbuhr <49747120+fredbuhr@users.noreply.github.com>
2026-09-09 10:40:20 +0200

    Block 3: activate live graph bridge in product shell

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,1 +1,11 @@
-export { default } from './app/KairoApp'
+import KairoApp from './app/KairoApp'
+import { LiveGraphBridge } from './features/graph/LiveGraphBridge'
+
+export default function App() {
+  return (
+    <>
+      <LiveGraphBridge />
+      <KairoApp />
+    </>
+  )
+}
